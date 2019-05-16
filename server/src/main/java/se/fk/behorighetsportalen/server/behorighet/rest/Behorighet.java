6ce720e94a81a9a00fe5b2b39f7e40621e65d09a
Martin Gunnarsson <martin.gunnarsson@forsakringskassan.se>
2019-05-16 15:12:00 +0200

    Klass för Behörighet och User

---
+++ server/src/main/java/se/fk/behorighetsportalen/server/behorighet/rest/Behorighet.java
@@ -0,0 +1,61 @@
+package se.fk.behorighetsportalen.server.behorighet.rest;
+
+import se.fk.behorighetsportalen.server.user.rest.User;
+
+import java.util.Arrays;
+
+public class Behorighet {
+    private String id;
+    private String[] kategorier;
+    private String beskrivning;
+    private User granskare;
+
+    public Behorighet(String id, String[] kategorier, String beskrivning, User granskare) {
+        this.id = id;
+        this.kategorier = kategorier;
+        this.beskrivning = beskrivning;
+        this.granskare = granskare;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getKategorier() {
+        return kategorier;
+    }
+
+    public void setKategorier(String[] kategorier) {
+        this.kategorier = kategorier;
+    }
+
+    public String getBeskrivning() {
+        return beskrivning;
+    }
+
+    public void setBeskrivning(String beskrivning) {
+        this.beskrivning = beskrivning;
+    }
+
+    public User getGranskare() {
+        return granskare;
+    }
+
+    public void setGranskare(User granskare) {
+        this.granskare = granskare;
+    }
+
+    @Override
+    public String toString() {
+        return "Behorighet{" +
+                "id='" + id + '\'' +
+                ", kategorier=" + Arrays.toString(kategorier) +
+                ", beskrivning='" + beskrivning + '\'' +
+                ", granskare=" + granskare +
+                '}';
+    }
+}
