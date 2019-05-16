ae365d55ddfe720257414f1b10e09c0489875eee
Jocke <joakim.edvinson@forsakringskassan.se>
2019-05-16 14:06:17 +0200

    Beskrivning lägg till i dropdown

--- gui/src/pages/SokBehorighet.tsx
+++ gui/src/pages/SokBehorighet.tsx
@@ -1,31 +1,48 @@
 import * as React from "react";
-import { Header, Container, Grid, Segment, Search } from "semantic-ui-react"; 
+import { Header, Container, Grid, Segment, Search, Dropdown } from "semantic-ui-react"; 
+import BehorigheterAllUser from "./Behorigheter-all-user";
+
+const mockResults = [{  
+  key: "Behörighet1",
+  value: "Behörighet1",
+  text: "Behörighetsbeskrivning"},
+  {key: "Behörighet2",
+  value: "Behörighet2",
+  text: "Behörighetsbeskrivning2 :D"}]
 
 interface Props {
 }
 
 interface State {
-  isLoading: false,
-  results: [],
-   value: '';
+  isLoading: boolean,
+  results: Array<any>,
+   value: any,
+  
 }
-const initialState = { isLoading: false, results: [] as Array<any>, value: '' }
+const initialState = { isLoading: false, results: [] as Array<any>, value: '', }
 
 export class SokBehorighet extends React.Component<State, Props> {
 
-    state = initialState
-    
+    state = {isLoading: false,
+    results: [] as Array<any>,
+    value: 'Sök...'}
+
 
     render() {
+      
         return (
             <Container>
                     <Header as={"h1"}>Sök Behörighet</Header>
-          <Search
+          <Dropdown
             fluid
-            loading={this.state.isLoading}
-            onResultSelect={() => ("")}
-            onSearchChange={() => ("")}
-            results={[]}
+            search
+            selection
+            options={mockResults}
+          
+
+          
+          
+          
           />
 
       </Container>
