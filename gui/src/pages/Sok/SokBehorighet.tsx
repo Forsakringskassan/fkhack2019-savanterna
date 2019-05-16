27dcad0f9b6a675fada474a8450d5c7d1e8bbb49
Martin Gunnarsson <martin.gunnarsson@forsakringskassan.se>
2019-05-16 21:30:40 +0200

    Fixade utsökning av behörighet

--- gui/src/pages/Sok/SokBehorighet.tsx
+++ gui/src/pages/Sok/SokBehorighet.tsx
@@ -1,64 +1,110 @@
 import * as React from "react";
-import { Header, Container, Dropdown } from "semantic-ui-react"; 
-
-const mockResults = [{  
-  key: "GIT User",
-  value: "Git user",
-  text: "Git user - Behörighet för att komma åt GIT"
-  },
-  {
-  key: "Stash user",
-  value: "Stash user",
-  text: "Stash user - Behörighet för att komma åt Stash."
-},
-{
-  key: "BLIP user",
-  value: "BLIP user",
-  text: "BLIP user - Behörighet för att använda BLIP",
-},
-{
-  key: "BLIP Admin",
-  value: "BLIP Admin",
-  text: "BLIP Admin -Behörighet för att administrera BLIP",
-},{
-  key: "Citrix user",
-  value: "Citrix user",
-  text: "Citrix user - Behörighet för åtkomst till Citrix",
-}]
+import { Card, Table, List, Label, Container, Header, Input, Button } from "semantic-ui-react"; 
+import RestInterface from "../../rest/rest-interface";
 
 interface Props {
 }
 
 interface State {
   isLoading: boolean,
-  results: Array<any>,
-   value: any,
-  
+  results: Array<RestInterface.Behorighet>,
+  value: any,  
 }
 
-export class SokBehorighet extends React.Component<State, Props> {
+export class SokBehorighet extends React.Component<Props, State> {
+
+  b1: RestInterface.Behorighet = {
+    id: "1", 
+    namn: "GStashUser", 
+    beskrivning: "Ger läsrättigheter till Stash", 
+    kategorier: [{id: "1", namn: "Systemutvecklare"}],
+    granskare: {id: "1", namn: "Kalle Karlsson"}  
+  };
+  b2: RestInterface.Behorighet = {
+    id: "2", 
+    namn: "GConfluenceUser", 
+    beskrivning: "Ger läsrättigheter till Confluence", 
+    kategorier: [{id: "1", namn: "Systemutvecklare"}, {id: "2", namn: "IT-Samordnare"}],
+    granskare: {id: "2", namn: "Anna Andersson"}  
+  };
+  b3: RestInterface.Behorighet = {
+    id: "3", 
+    namn: "GSuperUserWin10", 
+    beskrivning: "Ger sudo-rättigheter till Windows 10", 
+    kategorier: [{id: "1", namn: "Systemutvecklare"}],
+    granskare: {id: "2", namn: "Anna Andersson"}  
+  };
 
   constructor(props: any){
     super(props);
     this.state = {
       isLoading: false,
-      results: [],
+      results: [this.b1, this.b2, this.b3],
       value: ""
     };    
+  }
+
+  handleChange(event: any, data: any){
+    this.setState({
+      value: data.value
+    });
+  }
+
+  getResults(){
+    let results = this.state.results;
+    let search = this.state.value.toLowerCase();
+
+    let filteredData : Array<RestInterface.Behorighet> = results.filter(behorighet => 
+      behorighet.namn.toLowerCase().includes(search) ||
+      behorighet.beskrivning.toLowerCase().includes(search) ||
+      behorighet.kategorier.some(kategori => kategori.namn.toLowerCase().includes(search))
+    );
+
+    let rows = filteredData.map(item => (
+      <Table.Row key={item.id + "row"}>
+        <Table.Cell content={item.namn} key={item.id + item.namn}/>
+        <Table.Cell content={item.beskrivning} key={item.id + item.beskrivning}/>
+        <Table.Cell>
+          <List key={item.id + "kategorier"}>
+            {item.kategorier.map((kategori) =>
+                <List.Item key={item.id + kategori.id}>
+                    <Label content={kategori.namn}/>
+                </List.Item>
+            )}
+          </List>
+        </Table.Cell>
+        <Table.Cell key={item.id + "beställ"}><Button positive content={"Beställ"}/></Table.Cell>
+      </Table.Row>
+    ));
+    
+    return(
+      <div>
+        <Header as={"h2"} dividing>Resultat:</Header>        
+        <Table padded key={"results table"}>          
+          <Table.Header>
+            <Table.Row key={"header row"}>
+              <Table.HeaderCell content={"Gruppnamn"} key={"gruppnamn header"}/>
+              <Table.HeaderCell content={"Beskrivning"} key={"beskrivning header"}/>
+              <Table.HeaderCell content={"Kategorier"} key={"kategorier header"}/>
+              <Table.HeaderCell content={"Beställ"} key={"beställ header"}/>
+            </Table.Row>
+          </Table.Header>          
+          <Table.Body>
+            {rows}            
+          </Table.Body>          
+        </Table>
+      </div>
+    )
+      
   }
 
   render() {    
     return (
       <Container>
         <Header as={"h1"} dividing>Sök Behörighet</Header>
-        <Dropdown
-          fluid
-          search
-          selection
-          options={mockResults}
-          scrolling
-          placeholder={"Sök..."}
-        />
+        <Input fluid icon={'search'} placeholder={'Sök...'} onChange={this.handleChange.bind(this)}/>
+        <br/>
+        {this.state.value.length > 0 ? this.getResults() : null}
 
       </Container>
     )
